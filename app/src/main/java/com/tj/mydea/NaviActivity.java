b3a37c54eb435817cb0dcd15059e07eb47d91b6e
ctang <tommy9695@gmail.com>
2016-10-06 15:18:14 -0400

    Moved login to new activity. Now login links to NaviActivity

--- app/src/main/java/com/tj/mydea/NaviActivity.java
+++ app/src/main/java/com/tj/mydea/NaviActivity.java
@@ -1,14 +1,5 @@
 package com.tj.mydea;
-import java.util.Arrays;
-import java.util.List;
-import org.json.*;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.io.*;
-import android.util.Log;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -25,73 +16,17 @@ import android.view.MenuItem;
 import android.content.Intent;
 import android.widget.Toast;
 import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
-import com.facebook.login.LoginFragment;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
-import com.facebook.login.LoginManager;
-import com.facebook.*;
 
 
 public class NaviActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private CallbackManager callbackManager;
-    LoginButton loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_navi);
 
-        callbackManager = CallbackManager.Factory.create();
-        loginButton = (LoginButton)findViewById(R.id.login_button);
-        loginButton.setReadPermissions(Arrays.asList("public_profile", "email"));
-
-        loginButton.registerCallback(callbackManager,
-                new FacebookCallback<LoginResult>() {
-                    @Override
-                    public void onSuccess(LoginResult loginResult) {
-                        GraphRequest request = GraphRequest.newMeRequest(
-                                loginResult.getAccessToken(),
-                                new GraphRequest.GraphJSONObjectCallback() {
-                                    @Override
-                                    public void onCompleted(JSONObject object, GraphResponse response) {
-                                        try {
-                                            object.put("user_id", object.get("id"));
-                                            object.remove("id");
-                                            object.put("user_name", object.get("name"));
-                                            object.remove("name");
-                                            postUser(object);
-                                            Log.v("LoginActivity", object.toString());
-                                        }
-                                        catch (JSONException e) {Log.v("LoginActivity", e.toString());}
-                                    }
-                                });
-                        Bundle parameters = new Bundle();
-                        parameters.putString("fields", "id,name,email");
-                        request.setParameters(parameters);
-                        request.executeAsync();
-                    }
-
-                    @Override
-                    public void onCancel() {
-                        Toast toast = Toast.makeText(getApplicationContext(), "Login cancelled.", Toast.LENGTH_SHORT);
-                        toast.show();
-                    }
-
-                    @Override
-                    public void onError(FacebookException exception) {
-                        Toast toast = Toast.makeText(getApplicationContext(), "There was an error. Please try again later.", Toast.LENGTH_SHORT);
-                        toast.show();
-                    }
-                });
-
-        AppEventsLogger.activateApp(this);
+        Intent intent = getIntent();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -114,11 +49,11 @@ public class NaviActivity extends AppCompatActivity
 
     }
 
-    @Override
+    /*@Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
-    }
+    }*/
 
     @Override
     public void onBackPressed() {
@@ -168,10 +103,6 @@ public class NaviActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_messages) {
 
-        } else if (id == R.id.nav_login) {
-            Login LoginFragment = new Login();
-            FragmentManager manager = getSupportFragmentManager();
-            manager.beginTransaction().replace(R.id.layout_for_fragments, LoginFragment).commit();
         } else if (id == R.id.nav_send) {
 
         }
@@ -179,47 +110,5 @@ public class NaviActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
-    }
-
-    public void postUser(final JSONObject object) {
-        Thread t = new Thread(new Runnable() {
-            public void run() {
-                try {
-                    String query = "https://mydea-db.herokuapp.com/sendUser";
-
-                    URL url = new URL(query);
-                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                    conn.setConnectTimeout(5000);
-                    conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-                    conn.setDoOutput(true);
-                    conn.setDoInput(true);
-                    conn.setRequestMethod("POST");
-
-                    OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-                    wr.write(object.toString());
-                    wr.flush();
-
-                    if (conn.getResponseCode() != 200) {
-                        throw new RuntimeException("Failed : HTTP error code : "
-                                + conn.getResponseCode());
-                    }
-
-                    BufferedReader br = new BufferedReader(new InputStreamReader(
-                            (conn.getInputStream())));
-
-                    String output;
-                    System.out.println("Output from Server .... \n");
-                    while ((output = br.readLine()) != null) {
-                        System.out.println(output);
-                    }
-
-                    conn.disconnect();
-
-                }
-                catch (IOException e) {Log.v("LoginActivity", e.toString());}
-            }
-        });
-
-        t.start();
     }
 }
